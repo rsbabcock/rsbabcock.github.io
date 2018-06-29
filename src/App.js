950042a9b48fb2990a0665a7dd95971be20a734c
rsbabcock <rachael.s.babcock@gmail.com>
2018-06-29 09:52:22 -0500

    added react flip pages and redoing functions

--- src/App.js
+++ src/App.js
@@ -8,92 +8,117 @@ import NavBar from './nav/NavBar';
 
 class App extends Component {
 
-    // Set initial state
-    state = {
-        // currentView: "login",
-        // activeUser: localStorage.getItem("geoId"),
-        userScore: 0,
-        animals: [],
-        continents: [],
-        counter: 0,
-        buzzer: ''
+  // Set initial state
+  state = {
+    me: [],
+    projects: [],
+    favorites: [],
+    places: []
+  }
+
+
+
+  // Argument can be an event (via NavBar) or a string (via Login)
+  showView = function (e) {
+    let view = null
+
+    // Click event triggered switching view
+    if (e.hasOwnProperty("target")) {
+      view = e.target.id.split("__")[1]
+
+      // View switch manually triggered by passing in string
+    } else {
+      view = e
     }
+    // Update state to correct view will be rendered
+    this.setState({
+      currentView: view,
+    })
 
-
-  
-    // Argument can be an event (via NavBar) or a string (via Login)
-    showView = function (e) {
-        let view = null
-
-        // Click event triggered switching view
-        if (e.hasOwnProperty("target")) {
-            view = e.target.id.split("__")[1]
-
-            // View switch manually triggered by passing in string
-        } else {
-            view = e
-        }
-        // Update state to correct view will be rendered
+  }.bind(this)
+  // get annimals
+  // function to get my info
+  getMe = () => {
+    fetch("http://localhost:8088/me")
+      .then(r => r.json())
+      .then(me => {
         this.setState({
-            currentView: view,
+          me: me
         })
+      })
+  }
+  // function to get my projects
+  getProjects = () => {
+    fetch("http://localhost:8088/projects")
+      .then(r => r.json())
+      .then(projects => {
+        this.setState({
+          projects: projects
+        })
+      })
+  }
+  // function to get my favorite things
+  getFavorites = () => {
+    fetch("http://localhost:8088/favorites")
+      .then(r => r.json())
+      .then(favorites => {
+        this.setState({
+          favorites: favorites
+        })
+      })
+  }
+  // function to get places I've visited
+  getPlaces = () => {
+    fetch("http://localhost:8088/places")
+      .then(r => r.json())
+      .then(places => {
+        this.setState({
+          places: places
+        })
+      })
+  }
 
-    }.bind(this)
-    // get annimals
-    getAnimals = () => {
-        fetch("http://localhost:8088/animals")
-            .then(r => r.json())
-            .then(animals => {
-                let randomals = animals
-                // takes array of animals and randomizes in a shuffle method so that they don't repeat 
-                for (let i = randomals.length - 1; i > 0; i--) {
-                    const j = Math.floor(Math.random() * (i + 1));
-                    [randomals[i], randomals[j]] = [randomals[j], randomals[i]];
-                }
-                this.setState({
-                    animals: randomals
-                })
-            })
+  // function to handle game play
+
+
+  // Component that gets all animal and continent info
+  componentDidMount() {
+    this.getMe()
+    this.getProjects()
+    this.getFavorites()
+    this.getPlaces()
+
+  }
+
+
+  View = () => {
+    switch (this.state.currentView) {
+      case "gameScore":
+        return <GameScore score={this.state.userScore} activeUser={this.state.activeUser} counter={this.state.counter} showView={this.randomizeHandler} />
+      case "scoreList":
+        return <ScoreList activeUser={this.state.activeUser} />
+      case "welcome":
+        return <Welcome activeUser={this.state.activeUser} showView={this.showView} />
+      default:
+        return <Welcome activeUser={this.state.activeUser} showView={this.showView} />
     }
-    // function to get all continenet info
+  }
+}
 
-    // function to handle game play
-  
+render() {
+  return (
+    <article>
+      <NavBar viewHandler={this.showView}
+        activeUser={this.state.activeUser}
+        setActiveUser={this.setActiveUser}
+        randomizeHandler={this.state.randomizeHandler}
+      // profileHandler={this.viewProfile}
+      />
 
-    // Component that gets all animal and continent info
-    componentDidMount() {
-    
-    }
-
-
-    View = () => {
-            switch (this.state.currentView) {
-                case "gameScore":
-                    return <GameScore score={this.state.userScore} activeUser={this.state.activeUser} counter={this.state.counter} showView={this.randomizeHandler} />
-                case "scoreList":
-                    return <ScoreList activeUser={this.state.activeUser} />
-                case "welcome":
-                    return <Welcome activeUser={this.state.activeUser} showView={this.showView} />
-                default:
-                    return <Welcome activeUser={this.state.activeUser} showView={this.showView} />
-            }
-        }
-    }
-
-    render() {
-        return (
-            <article>
-                <NavBar viewHandler={this.showView}
-                    activeUser={this.state.activeUser}
-                    setActiveUser={this.setActiveUser}
-                    randomizeHandler={this.state.randomizeHandler}
-                // profileHandler={this.viewProfile}
-                />
-
-                {this.View()}
-            </article>
-        )
-    }
+      {this.View()}
+    </article>
+  )
+}
 }
 
 
