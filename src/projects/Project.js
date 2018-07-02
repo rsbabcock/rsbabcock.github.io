cce342e189c6f61afb84b4fb9ff9237ea2dcadb7
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-02 11:52:19 -0500

    project content hovering beautifully

--- src/projects/Project.js
+++ src/projects/Project.js
@@ -6,10 +6,13 @@ import './projectList.css'
 class Project extends Component {
     render() {
         return (
-            <article className="project">
+            <article className="project" id={`${this.props.name}__${this.props.id}`}>
+                {/* <h1 className="invisible">invisible</h1> */}
+                <div id={`project__${this.props.name}`}>
                 <p>{ this.props.name }</p>
                 <p>{ this.props.description }</p>
                 <a href={this.props.url} target="_blank">{this.props.url}</a>
+                </div>
             </article>
         )
     }
--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -9,13 +9,17 @@ class ProjectList extends Component {
     render() {
         return (
             <div className="projectList">
-            <div>
+            <div className="first__page">
+                <h1> My Projects </h1>
+            </div>
+            <div className="second__page">
                 {this.props.projects.map(project => (
                     <div key={project.id}>
                     <Project name={project.name}
                              description={project.description}
                              url={project.url}
-                             key={this.props.key} />
+                             key={this.props.key}
+                             id={project.id} />
                              </div>
                 ))}
             </div>
