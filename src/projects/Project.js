8dd39b35d06751d959e58a1820b798c35625f3f9
rsbabcock <rachael.s.babcock@gmail.com>
2018-07-09 15:27:42 -0500

    added some transparency

--- src/projects/Project.js
+++ src/projects/Project.js
@@ -9,7 +9,7 @@ class Project extends Component {
             <article className="project" id={`${this.props.name}__${this.props.id}`}>
                 {/* <h1 className="invisible">invisible</h1> */}
                 <div id={`project__${this.props.name}`}>
-                <p>{ this.props.name }</p>
+                <h1>{ this.props.name }</h1>
                 <p>{ this.props.description }</p>
                 <a href={this.props.url} target="_blank">Link</a>
                 </div>
